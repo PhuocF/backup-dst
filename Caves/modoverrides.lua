98e16a3a54a612f411647e01cd57145836c9aa54
Phuoc <phuoc@nftseedlab.com>
2024-02-27 16:03:09 +0700

    second commit

--- Caves/modoverrides.lua
+++ Caves/modoverrides.lua
@@ -5,6 +5,5 @@ return {
 ["workshop-1207269058"] = { enabled = true },
 ["workshop-375859599"] = { enabled = true },
 ["workshop-481765222"] = { enabled = true },
-["workshop-1852257480"] = { enabled = true },
-["workshop-2158755579"] = { enabled = true }
+["workshop-1852257480"] = { enabled = true }
 }
